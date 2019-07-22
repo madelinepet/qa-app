d46f17d2f92d2a36229d2ae704ec37865da5fd3e
Madi Peters <madi.peters@S05560-mbpr.local>
2019-07-22 14:39:22 -0700

    Finished tutorial, next I will connect up a real postgres backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {Route} from 'react-router-dom';
 import Callback from './Callback';
 
 import NavBar from './NavBar/NavBar';
@@ -7,10 +6,32 @@ import Questions from './Questions/Questions';
 import Question from './Question/Question';
 import NewQuestion from './NewQuestion/NewQuestion';
 import SecuredRoute from './SecuredRoute/SecuredRoute';
+import {Route, withRouter} from 'react-router-dom';
+import auth0Client from './Auth';
 
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkingSession: true,
+    }
+  }
+
+  async componentDidMount() {
+    if (this.props.location.pathname === '/callback') {
+      this.setState({checkingSession:false});
+      return;
+    }
+    try {
+      await auth0Client.silentAuth();
+      this.forceUpdate();
+    } catch (err) {
+      if (err.error !== 'login_required') console.log(err.error);
+    }
+    this.setState({checkingSession:false});
+  }
   render () {
     return (
       <div>
@@ -18,10 +39,12 @@ class App extends Component {
           <Route exact path='/' component={Questions}/>
           <Route exact path='/question/:questionId' component={Question}/>
           <Route exact path='/callback' component={Callback}/>
-          <SecuredRoute path='/new-question' component={NewQuestion} />
+          <SecuredRoute path='/new-question'
+                  component={NewQuestion}
+                  checkingSession={this.state.checkingSession} />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
